b96da1ce5ee206c06f937c1476ed22f978b7f993
Evan Liu <liuyf24@mails.tsinghua.edu.cn>
2026-02-02 09:52:24 +0000

    feat: add train loop

--- train.py
+++ train.py
@@ -15,9 +15,12 @@ encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: ''.join([itos[i] for i in l])
 # print(decode(encode('Hello World!')))
 
+device = 'cuda'
+
 import torch
-data = torch.tensor(encode(text), dtype=torch.long)
+data = torch.tensor(encode(text), dtype=torch.long, device=device)
 # print(data[:200])
+print("Data loaded")
 
 # split
 n = int(0.9 * len(data))
@@ -25,11 +28,11 @@ train_data = data[:n]
 test_data = data[n:]
 
 block_size = 8
-batch_size = 4
+batch_size = 32
 
 def get_batch(split):
     data = train_data if split == 'train' else test_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
+    ix = torch.randint(len(data) - block_size, (batch_size,), device=device)
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+1+block_size] for i in ix])
     return x, y
@@ -37,4 +40,43 @@ def get_batch(split):
 from model import PicoGPT
 n_embed = 32
 model = PicoGPT(vocab_size, n_embed)
-# print(decode(model.generate(torch.zeros((1, 1), dtype=torch.long), max_new_tokens=100)[0].tolist()))
+model = model.to(device)
+
+def sample():
+    input = torch.zeros((1, 1), dtype=torch.long, device=device)
+    print(decode(model.generate(input, max_new_tokens=100)[0].tolist()))
+
+lr = 1e-3
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+
+@torch.no_grad()
+def estimate_loss():
+    model.eval()
+    esti_iter = 10
+    loss = 0
+    out = {}
+    for split in ['train', 'val']:
+        losses = torch.zeros(esti_iter)
+        for k in range(esti_iter):
+            xb, yb = get_batch(split)
+            _, loss = model(xb, yb)
+            losses[k] = loss
+        out[split] = losses.mean()
+    print(f"Loss in train set: {out['train']}, val set: {out['val']}")
+    model.train()
+
+print("Start to train...")
+train_iter = 20000
+eval_interval = 2000
+for iter in range(train_iter):
+    xb, yb = get_batch('train')
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+    if iter % eval_interval == 0:
+        print(f"Iter {iter}:")
+        estimate_loss()
+
+sample()
