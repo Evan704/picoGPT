a33766a098c70fd1b3308a9569a0d47428307959
Evan Liu <liuyf24@mails.tsinghua.edu.cn>
2026-02-07 15:42:03 +0000

    feat: add info log for sample.py

--- sample.py
+++ sample.py
@@ -11,6 +11,7 @@ block_size = 256
 model = PicoGPT(vocab_size, n_embed, block_size)
 
 state_dict = torch.load('ckpt/best_model.pth', map_location='cuda')
+print("State dict loaded.")
 
 model.load_state_dict(state_dict)
 model.to(device='cuda')
